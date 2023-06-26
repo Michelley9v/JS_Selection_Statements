5582dc76bb081e3dd35eba7316f49798392b91eb
Michelle Hoffmann <michelley9v@yahoo.com>
2023-06-26 16:03:44 -0500

    Lesson-2-n-3

--- app.js
+++ app.js
@@ -2,3 +2,103 @@ console.log("Hello World!\n==========\n");
 console.log(
   "Follow the steps in the README.md file to complete the exercises:\n==========\n"
 );
+let answer = prompt ("what is your name");
+console.log ("Hello," = answer); //
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+let birthMonth = window prompt("What is your Birth Month?");
+
+switch (birthMonth) {
+ case "December":
+case "January":
+case "February":
+   console.log("Winter");
+  break;
+
+case "March":
+case "April":
+case "May":
+console.log("Spring");
+break;
+
+case "June":
+  case "July":
+    case "August":
+      console.log(Summer);
+      break;
+
+      case "September":
+        case "October":
+          case "November":
+            console.log (Fall);
+            break;
+}
+
+switch (typeId){
+  case "01":
+    type = "Tank Top";
+ break;
+ case "02":
+  type = "T-Shirt"
+  break;
+  case "03":
+    type = "Long Sleeve";
+    case "04":
+      type = "Sweat Shirt";
+      break;
+  default:
+    type = "Other";
+    break;
+}
+
+    switch (colorId){
+      case "Blk":
+      color = "Black";
+      break;
+case "Bl":
+  color = "Blue";
+  break;
+  case "RD":
+    color = "Red";
+    break;
+    case "PURP":
+      color = "purple"
+      break;
+      case "WH":
+        color = "White";
+        default:
+          type = "other";
+          break;
+    }
+
+switch (sizeId){
+ case "S":
+size = "small";
+ break;
+case  "M":
+size = "Medium";
+break;
+case "L":
+  size = "Large";
+  break;
+  case "XL":
+    size = "Extra-Large";
+    break;
+    case "One":
+    size =  "One size fits all";
+    break;
+}
+    console.log(`Product: ${size} ${color} ${type}`);
+  
